8765e95a26f2c332f915b0dfc046898123fa7c3a
brianchanbc <brianchanbc1@gmail.com>
2024-06-03 00:52:06 -0700

    bug fix on parfiles

--- include/image.hpp
+++ include/image.hpp
@@ -8,7 +8,6 @@ class Image {
 public:
     cv::Mat in;  //The original pixels before applying the effect
     cv::Mat out; //The updated pixels after applying the effect
-    cv::Rect bounds; //The size of the image
 
     Image(std::string filePath);
 
@@ -16,14 +15,11 @@ public:
 
     void save(std::string filePath);
 
-    static ushort clamp(double comp);
-
-    void grayscale();
+    static double clamp(double comp);
 
     void convolution(std::string effect);
 
     void convolutionParallel(std::string effect, int numThreads);
-
 private:
-    void convolutionBatchProcessing(int start, int end, std::string effect, bool greyscale, double *kernal, int filter[9][2], cv::Rect bounds, int width);
+    void convolutionBatchProcessing(int start, int end, std::string effect, bool greyscale, double *kernal, int filter[9][2], int width);
 };
--- src/image.cpp
+++ src/image.cpp
@@ -3,7 +3,6 @@
 Image::Image(std::string filePath) {
 	in = cv::imread(filePath, cv::IMREAD_UNCHANGED);
 	out = cv::Mat::zeros(in.size(), in.type());
-	bounds = cv::Rect(0, 0, in.cols, in.rows);
 }
 
 void Image::swapInOutPointers() {
@@ -14,22 +13,8 @@ void Image::save(std::string filePath) {
 	cv::imwrite(filePath, out);
 }
 
-ushort Image::clamp(double comp) {
+double Image::clamp(double comp) {
 	return std::min(255.0, std::max(0.0, comp));
-}
-
-void Image::grayscale() {
-	for (int y = bounds.y; y < bounds.height; y++) {
-		for (int x = bounds.x; x < bounds.width; x++) {
-			cv::Vec4b pixel = in.at<cv::Vec4b>(y, x);
-			double b = pixel[0];
-			double g = pixel[1];
-			double r = pixel[2];
-			double a = pixel[3];
-			double greyC = clamp((r + g + b) / 3);
-			out.at<cv::Vec4b>(y, x) = cv::Vec4b(greyC, greyC, greyC, a);
-		}
-	}
 }
 
 void Image::convolution(std::string effect) {
@@ -102,8 +87,8 @@ void Image::convolutionParallel(std::string effect, int numThreads) {
 	} else if (effect == "G") {
 		greyscale = true;
 	}
-	int numElements = (bounds.height - bounds.y) * (bounds.width - bounds.x);
-	int width = bounds.width - bounds.x + 1;
+	int numElements = in.rows * in.cols;
+	int width = in.cols;
 	int numElementsPerThread = ceil((double)numElements / numThreads);
 	std::vector<std::thread> threads;
 	for (int i = 0; i < numThreads; i++) {
@@ -115,19 +100,19 @@ void Image::convolutionParallel(std::string effect, int numThreads) {
 		if (chunkEnd > numElements) {
 			chunkEnd = numElements;
 		}
-		threads.push_back(std::thread([this](int start, int end, std::string effect, bool greyscale, double *kernal, int filter[9][2], cv::Rect bounds, int width) {
-			this->convolutionBatchProcessing(start, end, effect, greyscale, kernal, filter, bounds, width);
-		}, chunkStart, chunkEnd, effect, greyscale, kernal, filter, bounds, width));
+		threads.push_back(std::thread([this](int start, int end, std::string effect, bool greyscale, double *kernal, int filter[9][2], int width) {
+			this->convolutionBatchProcessing(start, end, effect, greyscale, kernal, filter, width);
+		}, chunkStart, chunkEnd, effect, greyscale, kernal, filter, width));
 	}
 	for (int i = 0; i < numThreads; i++) {
 		threads[i].join();
 	}
 }
 
-void Image::convolutionBatchProcessing(int start, int end, std::string effect, bool greyscale, double *kernal, int filter[9][2], cv::Rect bounds, int width) {
+void Image::convolutionBatchProcessing(int start, int end, std::string effect, bool greyscale, double *kernal, int filter[9][2], int width) {
 	for (int i = start; i < end; i++) {
-		int y = i / width + bounds.y;
-		int x = i % width + bounds.x;
+		int y = i / width;
+		int x = i % width;
 		if (greyscale) {
 			cv::Vec4b pixel = in.at<cv::Vec4b>(y, x);
 			double b = pixel[0];
@@ -145,7 +130,7 @@ void Image::convolutionBatchProcessing(int start, int end, std::string effect, b
 			for (int k = 0; k < 9; k++) {
 				int newY = y + filter[k][0];
 				int newX = x + filter[k][1];
-				if (newY >= bounds.y && newY < bounds.height && newX >= bounds.x && newX < bounds.width) {
+				if (newY >= 0 && newY < in.rows && newX >= 0 && newX < in.cols) {
 					cv::Vec4b pixel = in.at<cv::Vec4b>(newY, newX);
 					double b = pixel[0];
 					double g = pixel[1];
